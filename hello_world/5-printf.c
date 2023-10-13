5f1431c2ede781d080fcc0ecc37f0b43bd18df1d
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-13 14:11:43 -0400

    task #5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0 (Success)
+ * */
+int mani(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,)
+		return (0)
+}
+
