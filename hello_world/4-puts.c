43fb222ae7f27ffbb44365072f84ae36201a451d
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-13 14:05:56 -0400

    task #4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc puts(\"Programming is like building a multilingual puzzle)
+#include <stdio.h>
+/**
+ * main - Printing "Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
