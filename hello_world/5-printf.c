6e72e21f7d0f3aa40dd27472cf93aa56de186f4e
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-13 14:31:56 -0400

    task #5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - program prints with proper grammar, but the outcome is a piece of art,
  * Return: 0 (Success)
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
