fd001876548a38e47660792048b88fbb47531bad
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-16 13:25:18 -0400

    editing betty code style

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@
 int main (void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0) {
+	if(n > 0)
+	{
 		printf("%d is positive\n", n);
 	} else if(n < 0)
    	{
@@ -23,5 +23,4 @@ int main (void)
 	printf("0 is zero\n");
 	}
 	return (0);
-}
-	
+}	
