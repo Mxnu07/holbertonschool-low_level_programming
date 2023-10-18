c8edc9bdbbf89cb0ae8d99c12bc9574b8ebd6d8b
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-18 09:51:06 -0400

    print alphabet except 'e and q'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry poiny
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
