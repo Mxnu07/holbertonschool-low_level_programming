82973447c9bf693fb0b47752b83cfbdc730e991d
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-18 11:00:24 -0400

    task #9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n <= 9; n++)
+{
+	putchar((n % 10) + '0');
+	if (n != '9')
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
