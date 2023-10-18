c36e34cf59dee902489759ee91789c55d136ccbf
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-18 09:29:19 -0400

    programs that prints alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	putchar(alph);
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	putchar(alph);
+	putchar('\n');
+	return (0);
+}
