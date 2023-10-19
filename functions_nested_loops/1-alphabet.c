6734a4079f81b0600e75efa825a3d388df9fdf33
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-19 11:23:14 -0400

    task #1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - program that prints alphabet
- * Return: void
+ * main - This function prints the alphabet from 'a' to 'z' on the console
+ * Return: void (Success)
  */
 void print_alphabet(void)
 {
 char m;
 
 for (m = 'a'; m <= 'z'; m++)
-_putchar(m);
-_putchar('\n');
+{
+	_putchar(m);
+}
+	_putchar('\n');
 }
