6f7f814b5a53b9582d5f958316e6d2457fdfa841
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-19 10:45:10 -0400

    task #1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,10 +6,9 @@
  */
 void print_alphabet(void)
 {
-	char m;
-	
-	for (m = 'a'; m <= 'z'; m++)
-	_putchar(m);
+char m;
 
-	_putchar('\n');
+for (m = 'a'; m <= 'z'; m++)
+_putchar(m);
+_putchar('\n');
 }
