1be5162f1bbf6320109ee457708811ea24f53d59
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-19 13:46:24 -0400

    task #3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks for loweercase character
+ * Return: 1 if lowercase character or 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
