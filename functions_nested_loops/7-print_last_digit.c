eda07b81512303bb4e26d7e083301981f04b4cc5
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-22 20:43:16 -0400

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a num
+ * Return: last digit of value
+ * @a: number to valued
+ * @b: last digit of a
+ */
+int print_last_digit(int a)
+{
+	int b;
+	b = a % 10;
+
+	if (b < 0)
+	{
+		b = b * -1;
+	}
+		_putchar(b + '0');
+		return (b);
+}
