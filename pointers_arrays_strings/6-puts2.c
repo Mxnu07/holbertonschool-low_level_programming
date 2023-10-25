0085a863ee8693cc1db32d63e085b974e4551b94
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-25 10:41:05 -0400

    task#6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,15 +2,22 @@
 #include "main.h"
 /**
  * puts2 - function to print character of string
- * str: string to be printed
+ * @str: string to print
+ * Return: no return
  */
 void puts2(char *str)
 {
-	int m;
+	int count = 0;
 
-		for (m = 0; m < 9; m++)
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
 		{
-			putchar(*str);
+			putchar('\n');
+			break;
 		}
-	putchar('\n');
+		if (count % 2 == 0)
+			putchar(str[count]);
+		count++;
+	}
 }
