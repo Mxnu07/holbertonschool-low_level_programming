80e9df916d677260ebc36045b610344d3bc53a5e
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-29 20:23:38 -0400

    fixing style

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,8 @@
  * _strcmp - compares pointers to strings
  * @s1: first pointer to compare
  * @s2: second pointer to compare
- * Return: If str1 < str2, the negative difference of the first unmatched characters
+ * Return: If str1 < str2, the negative difference of the first
+ * unmatched characters
  *  If str1 == str2, 0
  *  If str1 > str2, the positive difference of the first unmatched characters
  */
