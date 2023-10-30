b285050d12dbf87de0b3a87cbbc5e5a6b9806a67
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-29 21:02:42 -0400

    last task

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers.
+ * print_diagsums - Prints sum of two diagonals of square matrix of integers.
  * @a: The matrix of integers.
  * @size: The size of the matrix.
  */
