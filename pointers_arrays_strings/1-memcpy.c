78dab980a39d193c780fb7c5dcc55e3ee1fe933c
MMD <manuelmoralesdiaz0@gmail.com>
2023-10-29 21:26:01 -0400

    fixing style

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,12 @@
  * @n: number of bytes to copy
  * Return: result of dest
  */
-char *_memcpy(char *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
