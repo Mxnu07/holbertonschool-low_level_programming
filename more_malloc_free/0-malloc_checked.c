1aa8ccc766ffb071eb70ca428a4cf3b892220be1
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-12 10:49:12 -0400

    task #0, #1 and #2

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @m: pointer
+ * @b: unisigned int
+ * Return: pointer to allocated memory, if fails return 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m;
+
+	m = malloc(b);
+
+	if (m == null)
+	{
+		exit(98);
+	}
+
+	return (m);
+}
