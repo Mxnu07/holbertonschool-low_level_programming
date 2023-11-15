a4fafef8c8e89641adfa962680c6690249de1037
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-15 12:05:57 -0400

    first task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
-
+/**
+ *  struct dog - dog info
+ *  @name: char type
+ *  @age: float type
+ *  @owner: char type
+ */
 struct dog
 {
-  char * name;
-  float age;
-  char * owner;
+har *name;
+float age;
+char *owner;
 };
+
+#endif
