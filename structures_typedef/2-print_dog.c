c4584cfda7e2455f3c8019f5d67f7cd281705df5
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-15 13:04:02 -0400

    adjusting betty to tasks

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,14 +1,16 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- *
+ * print_dog - print struct dog
+ * @d: pointer to struct dog
+ * Return: pointer to struct
  */
 
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name ==NULL)
+	if (d->name == NULL)
 		printf("Name: (nill)\n");
 	else
 		printf("Name: %s\n", d->name);
