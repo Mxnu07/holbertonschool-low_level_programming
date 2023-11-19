d62705a890b1e5602a9bd6ce16853a292f2424a9
MMD <manuelmoralesdiaz0@gmail.com>
2023-11-19 13:15:40 -0400

    task #0

---
+++ functions_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+void print_name(char *name, void (*f)(char *));
+
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
