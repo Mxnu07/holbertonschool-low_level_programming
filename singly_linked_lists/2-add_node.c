0773509855cade3bc5e55ac0f47cd5e7e7a22b9e
MMD <manuelmoralesdiaz0@gmail.com>
2023-12-01 21:49:46 -0400

    removing character for betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: A pointer to the head of the list_t list.
  * @str: The string to be added to the list_t list.
- * Return: If the function fails - NULL. otherwise - the address of the new element.
+ * Return: If fails return NULL. otherwise the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
